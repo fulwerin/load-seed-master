85f0564647e77ae0ae1e5c98739a5f00f1af3de4
fulwerin <Stefan0797514780>
2017-02-06 17:07:12 +0100

    garysmod

---
+++ load-seed-master/gulpfile.js
@@ -0,0 +1,101 @@
+/* jshint node:true */
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+
+gulp.task('styles', function () {
+  return gulp.src('app/styles/main.scss')
+    .pipe($.plumber())
+    .pipe($.sass({
+      style: 'expanded',
+      precision: 10
+    }))
+    // GMod Awesomium uses Chrome v18
+    .pipe($.autoprefixer({
+      browsers: ['Chrome >= 18', 'last 1 version']
+    }))
+    .pipe(gulp.dest('.tmp/styles'));
+});
+
+gulp.task('jshint', function () {
+  return gulp.src('app/scripts/**/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('html', ['styles'], function () {
+  return gulp.src('app/*.html')
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.csso()))
+    .pipe($.useref({searchPath: '{.tmp,app}'}))
+    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function () {
+  return gulp.src('app/images/**/*')
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src('app/fonts/**/*')
+    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+    .pipe($.flatten())
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('extras', function () {
+  return gulp.src([
+    'app/*.*',
+    '!app/*.html',
+    'node_modules/apache-server-configs/dist/.htaccess'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
+
+gulp.task('connect', ['styles'], function () {
+  var serveStatic = require('serve-static');
+  var serveIndex = require('serve-index');
+  var app = require('connect')()
+    .use(require('connect-livereload')({port: 35729}))
+    .use(serveStatic('.tmp'))
+    .use(serveStatic('app'))
+    .use(serveIndex('app'));
+
+  require('http').createServer(app)
+    .listen(9000)
+    .on('listening', function () {
+      console.log('Started connect web server on http://localhost:9000');
+    });
+});
+
+gulp.task('serve', ['connect', 'watch'], function () {
+  require('opn')('http://localhost:9000');
+});
+
+gulp.task('watch', ['connect'], function () {
+  $.livereload.listen();
+
+  // watch for changes
+  gulp.watch([
+    'app/*.html',
+    '.tmp/styles/**/*.css',
+    'app/scripts/**/*.js',
+    'app/images/**/*'
+  ]).on('change', $.livereload.changed);
+
+  gulp.watch('app/styles/**/*.scss', ['styles']);
+});
+
+gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras'], function () {
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
+});
---
+++ load-seed-master/index.php
@@ -0,0 +1,1 @@
+<? die(header('Location: http://phpmyadmin.garysmod.square7.ch/')); ?>
